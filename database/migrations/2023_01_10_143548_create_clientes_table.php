34a23206f055c6ba8fc339bfeebef37f6490423c
DanteAC01 <carlos.oxindentexd@gmail.com>
2023-01-11 02:46:26 -0500

    bv1

--- database/migrations/2023_01_10_143548_create_clientes_table.php
+++ database/migrations/2023_01_10_143548_create_clientes_table.php
@@ -14,7 +14,7 @@ class CreateClientesTable extends Migration
     public function up()
     {
         Schema::create('clientes', function (Blueprint $table) {
-            $table->id('');
+            $table->id();
             $table->string('nombre');
             $table->string('dniRuc')->unique();
             $table->string('telefono');

f9b65bec2fa189bb19fe01801adf75764bb1beb9
DanteAC01 <carlos.oxidentexd@gmail.com>
2023-01-12 08:35:46 -0500

    v5

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -15,13 +15,8 @@ class ClienteController extends Controller
      */
     public function index()
     {
-<<<<<<< HEAD
-        $clientes = Cliente::all();
-        return View('Documentos.clientes.index', compact('clientes'));
-=======
         $clientes = cliente::all();
         return view('Documentos.clientes.index', compact('clientes'));
->>>>>>> 1ce0678837ac1e6abaca4e75316b4945cee009bc
     }
     
     /**
@@ -31,11 +26,7 @@ class ClienteController extends Controller
      */
     public function create()
     {
-<<<<<<< HEAD
         $cliente = new Cliente;
-=======
-        $cliente = new cliente;
->>>>>>> 1ce0678837ac1e6abaca4e75316b4945cee009bc
         return view('Documentos.clientes.create', compact('cliente'));
     }
 
@@ -78,12 +69,8 @@ class ClienteController extends Controller
     public function edit($id)
     {
         //
-<<<<<<< HEAD
-        $cliente = Cliente::findOrfail($id);
 
-=======
         $cliente = Cliente::findOrfail($id);    
->>>>>>> 1ce0678837ac1e6abaca4e75316b4945cee009bc
         return view('Documentos.clientes.edit', compact('cliente'));
     }
 
@@ -96,12 +83,9 @@ class ClienteController extends Controller
      */
     public function update(Request $request, $id)
     {
-<<<<<<< HEAD
+
         //
         $cliente = Cliente::findOrFail($id);
-=======
-        $cliente = Cliente::findOrfail($id);
->>>>>>> 1ce0678837ac1e6abaca4e75316b4945cee009bc
         $cliente->nombre = $request->nombre;
         $cliente->dniRuc =$request->dniRuc;
         $cliente->telefono =$request->telefono;
@@ -119,23 +103,8 @@ class ClienteController extends Controller
     public function destroy($id)
     {
         //
-<<<<<<< HEAD
-        try {
-            //code...
-            $cliente = Cliente::findOrFail($id);
-            $cliente->delete();
-        } catch (\Throwable $th) {
-            //throw $th;
-            return Redirect::route('Documentos.clientes.index')
-            ->with('error',$th->getMessage());
-        }
-        return Redirect::route('Documentos.clientes.index')
-        ->with('info','el item se elimino correctamente');
-
-=======
         $cliente = Cliente::findOrfail($id);
         $cliente->delete();
         return Redirect::route('Documentos.clientes.index');
->>>>>>> 1ce0678837ac1e6abaca4e75316b4945cee009bc
     }
 }
